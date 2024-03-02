141b008cb882fb92f60da6bb9dd197b0ff19c3f2
Brundhahegde <151897970+Brundhahegde@users.noreply.github.com>
2024-03-02 16:08:27 +0530

    Add files via upload

---
+++ Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+ 
+export default function Header() {
+    return (
+        <div>
+            <header className="journal-header">
+                <h1 className="title">MY TRAVEL JOURNEY!!</h1>
+            </header>
+          {/* <div ><img src="https://wallpapers.com/images/hd/beach-view-on-full-screen-desktop-iwg20pvseh3n7oqd.jpg"/>
+        </div> */}
+        </div> 
+    )
+}
---
+++ data.js
@@ -0,0 +1,70 @@
+
+
+export const data = [
+	{
+		id: 1,
+		date: "May 2022",
+		title: "TajMahal",
+		description: 
+"The Taj Mahal is an ivory-white marble mausoleum on the south bank of the Yamuna river in the Indian city of Agra",
+
+		coverImage: "Tajmahal.jpg",
+		country: "INDIA",
+		googleMapLink: 
+"https://maps.app.goo.gl/c2LrakJifvshRje76",
+	},
+	{
+		id: 2,
+		date: "June 2022",
+		title: "kerala",
+		description: 
+"Named as one of the ten paradises of the world by National Geographic Traveler, Kerala is famous especially for its ecotourism initiatives and beautiful backwaters.",
+		coverImg: "kerala.jpg",
+		country: "INDIA",
+		googleMapLink: 
+"https://maps.app.goo.gl/Mo9yo8vgtDFLFobv6",
+	},
+    {
+		id: 3,
+		date: "July 2022",
+		title: "Hampi",
+		description: 
+"Hampi is one of the most beautiful historical sites in the history of India. It was the first capital of the Vijayanagara Empire, situated on the bank of River Tungabhadra",
+		coverImg: "hampi.jpg",
+		country: "INDIA",
+		googleMapLink: 
+"https://maps.app.goo.gl/C3W8FBEHffjySa4S9",
+    },
+    {
+        id: 4,
+		date: "September 2022",
+		title: "JogFalls",
+		description: 
+" Jog Falls is a waterfall on the Sharavati river located in Siddapura taluk, Uttara Kannada District and its view point located in Shimoga district of Karnataka, India.",
+		coverImg: "jogfalls.jpg",
+		country: "INDIA",
+		googleMapLink: 
+"https://maps.app.goo.gl/bcNMPJJXbrdx1YEo7",
+    },
+	{
+        id: 5,
+		date: "January 2023",
+		title: "Murudeshwar",
+		description: 
+"Murdeshwar is a town in Uttara Kannada district in the state of Karnataka, India, It is famous for the world's second tallest Shiva statue. ",
+		country: "INDIA",
+		googleMapLink: 
+"https://maps.app.goo.gl/6a8sgVnT2pgDNNcy6",
+    },
+	{
+        id: 6,
+		date: "February 2023",
+		title: "Mysore-Palace",
+		description: 
+"It is located in Mysore, Karnataka, India. It used to be the official residence of the Wadiyar dynasty and the seat of the Kingdom of Mysore ",
+		country: "INDIA",
+		googleMapLink: 
+"https://maps.app.goo.gl/9skbziiqLfgnBUnV7",
+    }
+	
+]
